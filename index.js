2871c02d4da64e0c43f660e3e4a8779f2a62190a
Eddy <eddypv0@gmail.com>
2021-05-17 11:26:46 -0400

    Install dependencies and config index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title:String,
+    author:String,
+    url:String,
+    likes:Number
+})
+const Blog = new mongoose.model('Blog', blogSchema)
+console.log(process.env.MONGO_URL)
+const mongoURL = process.env.MONGO_URL
+mongoose.connect(mongoURL, {
+    useNewUrlParser:true, 
+    useUnifiedTopology:true, 
+    useFindAndModify:false, 
+    useCreateIndex:true})
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs =>{
+            response.json(blogs)
+        })
+})
+
+
+
+const PORT = 3003
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
