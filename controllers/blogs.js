5dafd233a9fed3515d44362c8e0a52c081b8c653
Eddy <eddypv0@gmail.com>
2021-06-15 19:26:52 -0400

    [UPDATE] Practice 4.13 - 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,4 +18,41 @@ blogRouter.post('/', async (request, response, next)=> {
     
 })
 
+blogRouter.delete('/:id', async (request, response, next)=>{
+    try{
+        
+        const id = request.params.id
+        const blogDeleted = await Blog.findByIdAndDelete(id)
+        if(blogDeleted){
+            response.status(204).end()
+        }else{
+            next()
+        }
+        
+
+    }catch(error){
+        next(error)
+    }
+
+})
+
+blogRouter.put('/:id', async (request, response, next)=>{
+    try{
+        const blog = request.body
+        const id = request.params.id
+        const blogUpdated = await Blog.findByIdAndUpdate(id, blog, {new:true})
+        if(blogUpdated){
+            response.json(blogUpdated)
+        }else{
+            next()
+        }
+        
+
+    }catch(error){
+        next(error)
+    }
+
+})
+
+
 module.exports = blogRouter
